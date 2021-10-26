66bf8dfafe6d52e608a01e3a7631364acff6a46a
bran.new <bran.new@kakaopaycorp.com>
2021-10-26 13:32:30 +0900

    Chage Int

--- Gittul/ViewController.swift
+++ Gittul/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var viewModel: String!
+    private var viewModel: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
