ebb88697fc9270004fe6aa87a7b36f0254735c0b
bran.new <bran.new@kakaopaycorp.com>
2021-10-26 12:40:15 +0900

    Make ViewModel

--- Gittul/ViewController.swift
+++ Gittul/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var viewModel: String!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
